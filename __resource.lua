ba56a8a7cc5b278782caa0e3ab4ef1d2354c4370
Carmine <neos7@live.it>
2018-04-16 18:43:41 +0200

    Added config.ini and resource.lua to git

---
+++ __resource.lua
@@ -0,0 +1,14 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+file 'config.ini'
+
+client_scripts {
+	'nativeui.net.dll',
+	'vstancer_shared.net.dll',
+	'vstancer_client.net.dll'
+}
+
+server_scripts {
+	'vstancer_shared.net.dll',
+	'vstancer_server.net.dll'
+}
