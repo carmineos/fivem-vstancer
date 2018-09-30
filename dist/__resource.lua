f0bb078bb8a9b4a33a957d4d9674a07d746cc032
Carmine <neos7@live.it>
2018-09-30 19:47:53 +0200

    nothing important here

--- dist/__resource.lua
+++ dist/__resource.lua
@@ -1,8 +1,11 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
+--dependency 'NativeUI'
+
 file 'config.ini'
 
 client_scripts {
+	--'@NativeUI/NativeUI.net.dll',
 	'NativeUI.net.dll',
 	'Vstancer.Client.net.dll'
 }
