ad5be762405f45bde54f90712e4077fe37d49daa
Carmine <neos7@live.it>
2019-03-14 18:20:12 +0100

    Added reference to nuget package and updated to latest MenuAPI

--- dist/__resource.lua
+++ dist/__resource.lua
@@ -2,11 +2,13 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 --dependency 'MenuAPI'
 
-file 'config.ini'
+files {
+	--'@MenuAPI/MenuAPI.dll',
+	'MenuAPI.dll',
+	'config.ini'
+}
 
 client_scripts {
-	--'@MenuAPI/MenuAPI.net.dll',
-	'MenuAPI.net.dll',
 	'Vstancer.Client.net.dll'
 }
 
