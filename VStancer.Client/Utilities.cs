c8c84eab8a8fb4bb202d8816b4f8be427a2b4d90
Carmine Giugliano <neos7@live.it>
2020-02-23 18:37:38 +0100

    remove useless Helpers class

--- VStancer.Client/Utilities.cs
+++ VStancer.Client/Utilities.cs
@@ -26,21 +26,4 @@ namespace Vstancer.Client
             return GetEnumerator();
         }
     }
-
-    public static class Helpers
-    {
-        public static string RemoveByteOrderMarks(string xml)
-        {
-            /*
-            string bom = Encoding.UTF8.GetString(Encoding.UTF8.GetPreamble());
-            if (xml.StartsWith(bom))
-                xml = xml.Remove(0, bom.Length);
-            */
-
-            // Workaround 
-            if (!xml.StartsWith("<", StringComparison.Ordinal))
-                xml = xml.Substring(xml.IndexOf("<"));
-            return xml;
-        }
-    }
 }
